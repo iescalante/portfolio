9153421e3eced3cdf6b754dd4e6c4d2dc933550f
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-12-11 23:16:04 -0500

    basic structure added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Welcome to my portfolio!</div>;
+  return (
+    <>
+      <div className="landing-page">
+        <h1>Hi, I'm Ivan</h1>
+        <h2>I am a Full-Stack Web Developer</h2>
+      </div>
+      <div className="my-story">
+        <h2>About</h2>
+        <p>Blah blah blah</p>
+      </div>
+      <div className="tech-stack">
+        <h2>Tech Stack</h2>
+        <p>blah blah blah</p>
+      </div>
+      <div className="portfolio">
+        <h2>Portfolio</h2>
+        <p>blah blah blah</p>
+      </div>
+      <div className="contact">
+        <h2>Contact</h2>
+        <p>blah blah blah</p>
+        <p>Ivan Escalante © 2020 </p>
+      </div>
+    </>
+  );
 }
 
 export default App;
