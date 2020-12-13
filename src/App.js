776d4097c0d9ccd16fae795d94831e646782227f
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-12-13 18:03:44 -0500

    finished portfolio section and @media for navbar

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import reactLogo from "./assets/react-logo.png";
 import reduxLogo from "./assets/redux-logo.png";
 import mongodbLogo from "./assets/mongodb-logo.png";
 import gitLogo from "./assets/git-logo.png";
+import akullabelkaPreview from "./assets/akulla-belka-preview.png";
+import wearabologyPreview from "./assets/wearabology-preview.gif";
 import twitterLogo from "./assets/twitter-logo.png";
 import linkedinLogo from "./assets/linkedin-logo.png";
 import githubLogo from "./assets/github-logo.png";
@@ -20,6 +22,9 @@ function App() {
           Ivan Escalante
         </a>
         <nav className="nav-bar">
+          <div className="hamburger">
+            <div></div>
+          </div>
           <ul className="nav-bar menu">
             <li className="nav-bar links">
               <a href="#about">About</a>
@@ -43,17 +48,17 @@ function App() {
       <div id="about" className="about">
         <h2 className="title">About</h2>
         <section className="bio">
-          <div className="avatar"></div>
+          <div className="avatar" alt="Ivan's avatar"></div>
           <p>
             Born and raised in Montreal, Canada. Since I was a little kid, I was
             fascinated by technology and games, also martial arts! During my
             teenage years, I had given coding a shot (HTML and CSS) but
             abandoned pretty quickly when I saw that I was out of ideas and
-            didn't understand (lack of patience at that age :P). Life went on
+            didn't understand (lack of patience at that age...). Life went on
             and I decided to go towards Science (Biochemistry in particular).
           </p>
           <p>
-            After graduating in 2014 with a BSc. in Honors Biochemistry, I came
+            After graduating in 2014 with a BSc. Honors in Biochemistry, I came
             out of university ready to face the world! 5 years in, after going
             through several jobs, I was left lacking something, that feeling
             that I wasn't able to accomplish anything in my professional life.
@@ -70,8 +75,8 @@ function App() {
           </p>
           <p>
             You can also find me riding a bicycle during riding season and more
-            recently, I took up my motorcycle license to be able to ride
-            motorcycles as a newly-found hobby. Finally, I love Japan and its
+            recently, I acquired my motorcycle license to be able to ride
+            motorcycles as a newfound hobby. Finally, I love Japan and its
             culture. I was able to visit twice and always came back home wanting
             more of it!
           </p>
@@ -88,64 +93,113 @@ function App() {
         <h2 className="title">Tech Stack</h2>
         <section className="tech-list">
           <div className="tech-div">
-            <img src={htmlLogo} className="tech" />
+            <img src={htmlLogo} className="tech" alt="HTML logo" />
             <p>HTML</p>
           </div>
           <div className="tech-div">
-            <img src={cssLogo} className="tech" />
+            <img src={cssLogo} className="tech" alt="CSS logo" />
             <p>CSS</p>
           </div>
           <div className="tech-div">
-            <img src={jsLogo} className="tech" />
+            <img src={jsLogo} className="tech" alt="Javascript logo" />
             <p>Javascript</p>
           </div>
           <div className="tech-div">
-            <img src={nodejsLogo} className="tech" />
+            <img src={nodejsLogo} className="tech" alt="Node.js logo" />
             <p>Node.js</p>
           </div>
           <div className="tech-div">
-            <img src={expressLogo} className="tech" />
+            <img src={expressLogo} className="tech" alt="Express logo" />
             <p>Express</p>
           </div>
           <div className="tech-div">
-            <img src={reactLogo} className="tech" />
+            <img src={reactLogo} className="tech" alt="React logo" />
             <p>React</p>
           </div>
           <div className="tech-div">
-            <img src={reduxLogo} className="tech" />
+            <img src={reduxLogo} className="tech" alt="Redux logo" />
             <p>Redux</p>
           </div>
           <div className="tech-div">
-            <img src={mongodbLogo} className="tech" />
+            <img src={mongodbLogo} className="tech" alt="MongoDb logo" />
             <p>MongoDB</p>
           </div>
           <div className="tech-div">
-            <img src={gitLogo} className="tech" />
+            <img src={gitLogo} className="tech" alt="Git logo" />
             <p>Git</p>
           </div>
         </section>
       </div>
       <div id="portfolio" className="portfolio">
         <h2 className="title">Portfolio</h2>
-        <div className="akulla-belka"></div>
-        <div className="wearabology"></div>
+        <div className="project-div">
+          <img
+            src={akullabelkaPreview}
+            alt="A preview of the AkullaBelka app"
+            className="project-preview"
+          />
+          <div className="project-info">
+            <h2 className="project-title">AkullaBelka</h2>
+            <a href="https://akulla-belka.herokuapp.com/">DEMO</a>
+            <p className="project-description">
+              A small money-lending app that allows users to become borrowers
+              and/or lenders in order to exchange money between them with a late
+              payment tracker included. The technologies used are React.js,
+              Node.js, Express, MongoDB, Styled Components and Cron. I was able
+              to track the money throughout the app, applying Cron as a
+              scheduler for late payments and implemented MongoDB for the first
+              time in an app of this size.
+            </p>
+          </div>
+        </div>
+        <div className="project-div">
+          <img
+            src={wearabologyPreview}
+            alt="A preview of the Wearabology app"
+            className="project-preview"
+          />
+          <div className="project-info">
+            <h2 className="project-title">Wearabology</h2>
+            <a href="https://project-wearabology.herokuapp.com/">DEMO</a>
+            <p className="project-description">
+              An e-commerce app done for a team project which allows the user to
+              select items, add items to a cart, checkout and receive a
+              confirmation. I was in charge of the Back-End for the project and
+              made sure to provide the Front-End with all the data necessary
+              through the proper routes to be able to connect to Redux. Also,
+              tested React Spring in the Error Page. With this project, I was
+              able to greatly improve my BE skills in a short time.
+            </p>
+          </div>
+        </div>
+        <div className="project-div">
+          <p className="project-coming-soon">MORE TO COME!</p>
+        </div>
       </div>
       <div id="contact" className="contact">
         <h2 className="title">Contact</h2>
         <section className="social-media-list">
           <div className="social-media">
             <a href="https://twitter.com/IvanEnriqueEC">
-              <img src={twitterLogo} className="media-logo" />
+              <img
+                src={twitterLogo}
+                className="media-logo"
+                alt="Twitter logo"
+              />
             </a>
           </div>
           <div className="social-media">
             <a href="https://www.linkedin.com/in/ivan-enrique-escalante-cornejo/">
-              <img src={linkedinLogo} className="media-logo" />
+              <img
+                src={linkedinLogo}
+                className="media-logo"
+                alt="LinkedIn logo"
+              />
             </a>
           </div>
           <div className="social-media">
             <a href="https://www.github.com/iescalante">
-              <img src={githubLogo} className="media-logo" />
+              <img src={githubLogo} className="media-logo" alt="Github logo" />
             </a>
           </div>
         </section>
